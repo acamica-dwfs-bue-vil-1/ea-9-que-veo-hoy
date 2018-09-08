81c911ff819e23833c8e248aba99591af6b5a3ae
Eitán Abelson <llovia@gmail.com>
2018-09-08 09:10:46 -0300

    Se agregan variables de entorno.

--- servidor/lib/conexionbd.js
+++ servidor/lib/conexionbd.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: 'localhost', 
-  port: '3306',
-  user: 'root',
-  password : 'acamica0',
-  database : 'que_veo'
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || '3306',   
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 connection.connect();
