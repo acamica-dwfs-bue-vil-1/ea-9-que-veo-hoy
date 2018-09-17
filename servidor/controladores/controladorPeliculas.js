1d7c82b28be9cf7ebcbaa3f3638db2deec6f101f
Eitán Abelson <llovia@gmail.com>
2018-09-17 01:20:47 -0300

    Se completa la guia 2/3.

--- servidor/controladores/controladorPeliculas.js
+++ servidor/controladores/controladorPeliculas.js
@@ -2,10 +2,17 @@ var connection = require('../lib/conexionbd');
 
 function buscarPeliculas (req, res) {
   let sql = 'SELECT * FROM pelicula';
+  let sql_;
   let titulo = req.query.titulo;
   let anio = req.query.anio;
   let genero = req.query.genero;
+  let orden =req.query.columna_orden; 
+  let tipoOrden = req.query.tipo_orden;
+  let pagina = req.query.pagina;
+  let cantidad = req.query.cantidad;
+  let total;
 
+//FILTROS POR TÍTULO, AÑO Y GÉNERO
   if (titulo && anio && genero) {
     console.log(`título: ${titulo} \n año: ${anio} \n género: ${genero}`)
     sql += ` WHERE titulo LIKE \'\%${titulo}\%\' AND anio = ${anio} AND genero_id = ${genero}`;
@@ -30,16 +37,45 @@ function buscarPeliculas (req, res) {
     console.log('sólo género: ' + sql);
   }
 
+//FILTROS POR ORDEN
+  if (orden === 'anio') {
+    sql += ` ORDER BY anio ${tipoOrden}`;    
+    console.log(sql);
+  } else if (orden === 'puntuacion') {
+    sql += ` ORDER BY puntuacion ${tipoOrden}`;    
+    console.log(sql);
+  }
+  sql_ = sql;      
+  //PAGINACIÓN Y LIMIT
+  sql += ` LIMIT ${(pagina - 1) * cantidad},${cantidad}`;
+
   connection.query(sql, function(error, resultado, fields) {
     if (error) {
         console.log("Hubo un error en la consulta", error.message);
         return res.status(404).send("Hubo un error en la consulta");
+        console.log(sql);        
     }
-    var response = {
-        'peliculas': resultado
-    };
+    
+    //TOTAL DE PELÍCULAS
+    connection.query(sql_, function(error_, resultado_, fields_) {
+      if (error_) {
+        console.log("Hubo un error en la consulta", error_.message);
+        return res.status(404).send("Hubo un error en la consulta");       
+      }
+      total = resultado_.length;
+      console.log(`sql_ = ${sql_}`);
+      console.log(total);
 
-    res.send(JSON.stringify(response));
+      var response = {
+        'peliculas': resultado,
+        'total': total
+      };
+
+      res.send(JSON.stringify(response));
+      console.log(sql);
+      console.log(total);    
+    });
+
   });
 }
 
